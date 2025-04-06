f2ce2479b0247063eac3105c4538cb26f83082d1
joél hawkins torres <joel@joel.fm>
2025-04-06 15:15:04 -0700

    improve loading
    Signed-off-by: joél hawkins torres <joel@joel.fm>

--- zsh-fn.plugin.zsh
+++ zsh-fn.plugin.zsh
@@ -1,4 +1,12 @@
-#!/usr/bin/env zsh
-
+# Standardized $0 Handling
+# https://wiki.zshell.dev/community/zsh_plugin_standard#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
+
+# Functions Directory
+# https://wiki.zshell.dev/community/zsh_plugin_standard#funtions-directory
+if [[ $PMSPEC != *f* ]]; then
+  if [[ ${zsh_loaded_plugins[-1]} != */kalc && -z ${fpath[(r)${0:h}/functions]} ]]; then
+    fpath+=( "${0:h}/functions" )
+  fi
+fi
